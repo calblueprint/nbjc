b18ea4f09d16313d4b65d6d1281da8c029b8b53c
Cindy Zhang <cindyzhang0718@gmail.com>
2020-09-27 23:52:22 -0400

    fix(home): added a head title and changed layout title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Layout from 'components/Layout';
+import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => {
   return (
-    <Layout title="Organization List | Next.js + TypeScript Example">
+    <Layout title="Home Page | Next.js + TypeScript Example">
+      <Head>
+        <title>NBJC Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>Home</h1>
       <p>FRED IS THE IMPOSTER!!!!!</p>
     </Layout>
