21bba4c0c2b8caeb1ad21137b34f6e8a456556a0
Bryanna Gavino <bry_gavino01@berkeley.edu>
2020-10-04 14:31:30 -0700

    trying to format mapbox style2

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,3 +1,5 @@
+<script src='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
 import Layout from 'components/Layout';
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
