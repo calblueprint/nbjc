4edbd245f4d1ec01b913789a61a3aedaab1761d1
Frederick Kim <freddy.kim8@gmail.com>
2020-10-04 16:55:02 -0700

    fix(tsconfig.json): change typescript config to be strict true
    This tells typescript to be more strict when checking types allowing us to ensure that we are
    type-safe across our app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   useEffect(() => {
     // Remove the server-side injected CSS
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
+    if (jssStyles && jssStyles.parentElement) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
