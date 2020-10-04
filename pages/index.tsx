5555f67750c47da9e94bd26fb7074c364ac176c1
Bryanna Gavino <bry_gavino01@berkeley.edu>
2020-10-04 12:44:21 -0700

    made map page and trying to implement API

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,17 @@ const Home: React.FC = () => {
         Hello
       </Button>
       <CircularProgress />
+      <Map
+      style={{ mapbox: '//styles/mapbox/streets-v9' }}
+      containerStyle={{
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
+    </Map>
     </Layout>
   );
 };
