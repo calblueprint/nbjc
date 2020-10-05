6da501f77005e04fb6e2cd898bdc1dc9b120fadd
Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>
2020-10-04 23:37:38 -0400

    New Organization Table (#4)
    * feat(new organization table): added organization table in postgresql through db-migrate and prisma
    
    * fix: deleted many_users and one_app, changed name to be notNull
    
    * fix(changed org table to ts): change org table files to ts

---
+++ migrations/20201005031531-create-organizations.ts
@@ -0,0 +1,43 @@
+import Base from 'db-migrate-base';
+import { promisify } from 'util';
+
+/**
+ * Describe what your `up` migration does.
+ */
+export async function up(
+  db: Base,
+  callback: Base.CallbackFunction
+): Promise<void> {
+  const createTable = promisify(db.createTable.bind(db));
+  try {
+    await createTable('orgnizations', {
+      id: { type: 'int', primaryKey: true, autoIncrement: true },
+      name: { type: 'string', notNull: true },
+      long: 'decimal',
+      lat: 'decimal',
+      type: 'string',
+    });
+  } catch (err) {
+    callback(err, null);
+  }
+}
+
+/**
+ * Describe what your `down` migration does.
+ */
+export async function down(
+  db: Base,
+  callback: Base.CallbackFunction
+): Promise<void> {
+  const dropTable = promisify<string>(db.dropTable.bind(db));
+  try {
+    await dropTable('organizations');
+  } catch (err) {
+    callback(err, null);
+  }
+}
+
+// eslint-disable-next-line no-underscore-dangle
+export const _meta = {
+  version: 1,
+};
