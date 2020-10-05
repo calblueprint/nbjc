d8f86d1758b988e8e6a04541b8077a555484d6e2
Calvin Chen <chencalvin99@berkeley.edu>
2020-10-04 17:41:57 -0700

    feat(next-auth): adding next-auth framework

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import Layout from 'components/Layout';
 import { Button, CircularProgress } from '@material-ui/core';
-import styles from '../styles/Home.module.css';
+// import { signIn, signOut, useSession } from 'next-auth/client';
+// import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => {
+  // const [session, loading] = useSession();
   return (
+    //   <>
+    //   {!session && <>
+    //     Not signed in <br/>
+    //     <button onClick={signIn}>Sign in</button>
+    //   </>}
+    //   {session && <>
+    //     Signed in as {session.user.email} <br/>
+    //     <button onClick={signOut}>Sign out</button>
+    //   </>}
+    // </>
     <Layout title="NBJC Home">
       <h1>Home</h1>
       <p>FRED IS THE IMPOSTER!!!!!</p>
