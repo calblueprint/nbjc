2ba0bb79559430148f76fc7954b8202be8e4095f
Frederick Kim <freddy.kim8@gmail.com>
2020-10-10 13:56:53 -0700

    fix(next-auth): add types for next-auth
    added types for next-auth to work with typescript

--- migrations/20201005024201-create-authusers.ts
+++ migrations/20201005024201-create-authusers.ts
@@ -4,72 +4,73 @@ import { promisify } from 'util';
 export function up(db: Base, callback: Base.CallbackFunction): void {
   db.runSql(
     `
-CREATE TYPE user_role AS ENUM ('organization', 'moderator', 'admin');
-CREATE TABLE accounts
-  (
-    id                   SERIAL,
-    compound_id          VARCHAR(255) NOT NULL,
-    user_id              INTEGER NOT NULL,
-    provider_type        VARCHAR(255) NOT NULL,
-    provider_id          VARCHAR(255) NOT NULL,
-    provider_account_id  VARCHAR(255) NOT NULL,
-    refresh_token        TEXT,
-    access_token         TEXT,
-    access_token_expires TIMESTAMPTZ,
-    created_at           TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at           TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-  );
-CREATE TABLE sessions
-  (
-    id            SERIAL,
-    user_id       INTEGER NOT NULL,
-    expires       TIMESTAMPTZ NOT NULL,
-    session_token VARCHAR(255) NOT NULL,
-    access_token  VARCHAR(255) NOT NULL,
-    created_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-  );
-CREATE TABLE users
-  (
-    id             SERIAL,
-    first_name     VARCHAR(255),
-    last_name      VARCHAR(255),
-    role           user_role,
-    email          VARCHAR(255),
-    email_verified TIMESTAMPTZ,
-    image          VARCHAR(255),
-    created_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-  );
-CREATE TABLE verification_requests
-  (
-    id         SERIAL,
-    identifier VARCHAR(255) NOT NULL,
-    token      VARCHAR(255) NOT NULL,
-    expires    TIMESTAMPTZ NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-  );
-CREATE UNIQUE INDEX compound_id
-  ON accounts(compound_id);
-CREATE INDEX provider_account_id
-  ON accounts(provider_account_id);
-CREATE INDEX provider_id
-  ON accounts(provider_id);
-CREATE INDEX user_id
-  ON accounts(user_id);
-CREATE UNIQUE INDEX session_token
-  ON sessions(session_token);
-CREATE UNIQUE INDEX access_token
-  ON sessions(access_token);
-CREATE UNIQUE INDEX email
-  ON users(email);
-CREATE UNIQUE INDEX token
-  ON verification_requests(token);`,
+      CREATE TYPE user_role AS ENUM ('organization', 'moderator', 'admin');
+      CREATE TABLE accounts
+        (
+          id                   SERIAL,
+          compound_id          VARCHAR(255) NOT NULL,
+          user_id              INTEGER NOT NULL,
+          provider_type        VARCHAR(255) NOT NULL,
+          provider_id          VARCHAR(255) NOT NULL,
+          provider_account_id  VARCHAR(255) NOT NULL,
+          refresh_token        TEXT,
+          access_token         TEXT,
+          access_token_expires TIMESTAMPTZ,
+          created_at           TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at           TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        );
+      CREATE TABLE sessions
+        (
+          id            SERIAL,
+          user_id       INTEGER NOT NULL,
+          expires       TIMESTAMPTZ NOT NULL,
+          session_token VARCHAR(255) NOT NULL,
+          access_token  VARCHAR(255) NOT NULL,
+          created_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        );
+      CREATE TABLE users
+        (
+          id             SERIAL,
+          first_name     VARCHAR(255),
+          last_name      VARCHAR(255),
+          role           user_role,
+          email          VARCHAR(255),
+          email_verified TIMESTAMPTZ,
+          image          VARCHAR(255),
+          created_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at     TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        );
+      CREATE TABLE verification_requests
+        (
+          id         SERIAL,
+          identifier VARCHAR(255) NOT NULL,
+          token      VARCHAR(255) NOT NULL,
+          expires    TIMESTAMPTZ NOT NULL,
+          created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        );
+      CREATE UNIQUE INDEX compound_id
+        ON accounts(compound_id);
+      CREATE INDEX provider_account_id
+        ON accounts(provider_account_id);
+      CREATE INDEX provider_id
+        ON accounts(provider_id);
+      CREATE INDEX user_id
+        ON accounts(user_id);
+      CREATE UNIQUE INDEX session_token
+        ON sessions(session_token);
+      CREATE UNIQUE INDEX access_token
+        ON sessions(access_token);
+      CREATE UNIQUE INDEX email
+        ON users(email);
+      CREATE UNIQUE INDEX token
+        ON verification_requests(token);
+    `,
     callback
   );
 }
@@ -99,6 +100,7 @@ export async function down(
   } catch (err) {
     callback(err, null);
   }
+  db.runSql(`DROP TYPE user_role;`);
 }
 
 // eslint-disable-next-line no-underscore-dangle
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,20 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 const options = {
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials) => {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return Promise.resolve(user);
+        }
+        // If you return null or false then the credentials will be rejected
+        return Promise.resolve(null);
+        // You can also Reject this callback with an Error or with a URL:
+        // return Promise.reject(new Error('error message')) // Redirect to error page
+        // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+      },
     }),
-    // ...add more providers here
   ],
 
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
 };
 
-// export default (req, res) => {
-//   return NextAuth(req, res, options);
-// };
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  NextAuth(req, res, options);
+};
