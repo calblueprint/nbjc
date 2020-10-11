ad6cf65b63c754c996e0c6df81de256c6973e5f3
Cindy Zhang <cindyzhang7@berkeley.edu>
2020-10-11 15:06:28 -0400

    feat(org api): rebasing

--- pages/api/orgs/index.ts
+++ pages/api/orgs/index.ts
@@ -3,53 +3,49 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
+function handleCREATE(req, res): void {
+  const post = await prisma.organizations.create({
+    where: { id: Number(postId) },
+    include: { author: true },
+  });
+  res.json(post);
+}
+
+function handleDELETE(req, res): void {
+  const post = await prisma.organizations.delete({
+    where: { id: Number(postId) },
+  });
+  res.json(post);
+}
+
+function handleUPDATE(req, res): void {
+  const post = await prisma.post.findOne({
+    where: { id: Number(postId) },
+    include: { author: true },
+  });
+  res.json(post);
+}
+
+function handleREAD(req, res): void {
+  const post = await prisma.post.findOne({
+    where: { id: Number(postId) },
+    include: { author: true },
+  });
+  res.json(post);
+}
+
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-  
-    if (req.method === 'CREATE') {
-      handleCREATE(req, res);
-    } else if (req.method === 'DELETE') {
-      handleDELETE(req, res);
-    } else if (req.method === 'UPDATE') {
-        handleUPDATE(req, res);
-    }
-    else if (req.method === 'READ') {
-        handleREAD(req, res);
-    } else {
-      throw new Error(
-        `The HTTP ${req.method} method is not supported at this route.`
-      );
-    }
-  };
-
-function handleCREATE( req, res ) => {
-    const post = await prisma.orgnizations.create({
-      where: { id: Number(postId) },
-      include: { author: true },
-    })
-    res.json(post);
+  if (req.method === 'CREATE') {
+    handleCREATE(req, res);
+  } else if (req.method === 'DELETE') {
+    handleDELETE(req, res);
+  } else if (req.method === 'UPDATE') {
+    handleUPDATE(req, res);
+  } else if (req.method === 'READ') {
+    handleREAD(req, res);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
   }
-  
-  // DELETE /api/post/:id
-  function handleDELETE( req, res): void => {
-    const post = await prisma.organizations.delete({
-        where: { id: Number(postId) },
-    });
-    res.json(post);
-  }
-
-  function handleUPDATE(req, res): void => {
-    const post = await prisma.post.findOne({
-        where: { id: Number(postId) },
-        include: { author: true },
-    });
-    res.json(post);
-  }
-
-  function handleREAD(req, res): void => {
-    const post = await prisma.post.findOne({
-      where: { id: Number(postId) },
-      include: { author: true },
-    })
-    res.json(post);
-  }
-
+};
