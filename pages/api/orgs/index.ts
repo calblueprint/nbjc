66cdca1f4a574b1a53bb9b1cabc225672cbfbd28
Cindy Zhang <cindyzhang0718@gmail.com>
2020-10-11 00:59:24 -0400

    feat(org api): started 4 API calls

---
+++ pages/api/orgs/index.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  
+    if (req.method === 'CREATE') {
+      handleCREATE(req, res);
+    } else if (req.method === 'DELETE') {
+      handleDELETE(req, res);
+    } else if (req.method === 'UPDATE') {
+        handleUPDATE(req, res);
+    }
+    else if (req.method === 'READ') {
+        handleREAD(req, res);
+    } else {
+      throw new Error(
+        `The HTTP ${req.method} method is not supported at this route.`
+      );
+    }
+  };
+
+function handleCREATE( req, res ) => {
+    const post = await prisma.orgnizations.create({
+      where: { id: Number(postId) },
+      include: { author: true },
+    })
+    res.json(post)
+  }
+  
+  // DELETE /api/post/:id
+  function handleDELETE( req, res): void => {
+    const post = await prisma.organizations.delete({
+      where: { id: Number(postId) },
+    })
+    res.json(post)
+  }
+
+  function handleUPDATE(req, res): void => {
+    const post = await prisma.post.findOne({
+      where: { id: Number(postId) },
+      include: { author: true },
+    })
+    res.json(post)
+  }
+
+  function handleREAD(req, res): void => {
+    const post = await prisma.post.findOne({
+      where: { id: Number(postId) },
+      include: { author: true },
+    })
+    res.json(post)
+  }
+
