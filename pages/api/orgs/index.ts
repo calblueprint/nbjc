e7a59d4da968480bfa80ff148c7d3943ceacf188
Cindy Zhang <cindyzhang7@berkeley.edu>
2020-10-11 15:41:08 -0400

    feat(org pai): fixing lint

--- pages/api/orgs/index.ts
+++ pages/api/orgs/index.ts
@@ -3,38 +3,57 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
-function handleCREATE(req, res): void {
+async function handleCREATE(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   const post = await prisma.organizations.create({
-    where: { id: Number(postId) },
-    include: { author: true },
+    data: {
+      name: req.body.name,
+      long: req.body.long,
+      lat: req.body.lat,
+      type: req.body.type,
+    },
   });
   res.json(post);
 }
 
-function handleDELETE(req, res): void {
+async function handleDELETE(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   const post = await prisma.organizations.delete({
-    where: { id: Number(postId) },
+    where: { id: Number(req.id) },
   });
   res.json(post);
 }
 
-function handleUPDATE(req, res): void {
-  const post = await prisma.post.findOne({
-    where: { id: Number(postId) },
-    include: { author: true },
-  });
-  res.json(post);
-}
-
-function handleREAD(req, res): void {
-  const post = await prisma.post.findOne({
+async function handleUPDATE(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const post = await prisma.organizations.findOne({
     where: { id: Number(postId) },
     include: { author: true },
   });
   res.json(post);
 }
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
+async function handleREAD(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const post = await prisma.organizations.findOne({
+    where: { id: Number(postId) },
+    include: { author: true },
+  });
+  res.json(post);
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
   if (req.method === 'CREATE') {
     handleCREATE(req, res);
   } else if (req.method === 'DELETE') {
