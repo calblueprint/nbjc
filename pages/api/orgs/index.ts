f5c00efd76c87f1a23b7878f06236acafe530a93
Cindy Zhang <cindyzhang7@berkeley.edu>
2020-10-11 23:06:31 -0400

    feat(api works): org API works, dont get latest node yall

--- pages/api/orgs/index.ts
+++ pages/api/orgs/index.ts
@@ -7,65 +7,99 @@ async function handleCREATE(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const newOrg = await prisma.organizations.create({
-    data: {
-      name: req.body.name,
-      long: req.body.long,
-      lat: req.body.lat,
-      type: req.body.type,
-    },
-  });
-  res.json(newOrg);
+  if (req.body.name) {
+    try {
+      const newOrg = await prisma.organizations.create({
+        data: {
+          name: req.body.name,
+          long: req.body.long,
+          lat: req.body.lat,
+          type: req.body.type,
+        },
+      });
+      res.json(newOrg);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res
+      .status(500)
+      .json({ statusCode: 500, message: 'Org name not passed in.' });
+  }
 }
 
 async function handleDELETE(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const deletedOrg = await prisma.organizations.delete({
-    where: { id: Number(req.query.id) },
-  });
-  res.json(deletedOrg);
+  if (req.query.id) {
+    try {
+      const deletedOrg = await prisma.organizations.delete({
+        where: { id: Number(req.query.id) },
+      });
+      res.json(deletedOrg);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.status(500).json({ statusCode: 500, message: 'Org ID not passed in.' });
+  }
 }
 
-async function handleUPDATE(
+async function handlePATCH(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const updatedOrg = await prisma.organizations.update({
-    where: { id: Number(req.query.id) },
-    data: {
-      name: req.body.name,
-      long: req.body.long,
-      lat: req.body.lat,
-      type: req.body.type,
-    },
-  });
-  res.json(updatedOrg);
+  if (req.query.id) {
+    try {
+      const updatedOrg = await prisma.organizations.update({
+        where: { id: Number(req.query.id) },
+        data: {
+          name: req.body.name,
+          long: req.body.long,
+          lat: req.body.lat,
+          type: req.body.type,
+        },
+      });
+      res.json(updatedOrg);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.status(500).json({ statusCode: 500, message: 'Org ID not passed in.' });
+  }
 }
 
-async function handleREAD(
+async function handleGET(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const org = await prisma.organizations.findOne({
-    where: { id: Number(req.query.id) },
-  });
-  res.json(org);
+  if (req.query.id) {
+    try {
+      const org = await prisma.organizations.findOne({
+        where: { id: Number(req.query.id) },
+      });
+      res.json(org);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.status(500).json({ statusCode: 500, message: 'Org ID not passed in.' });
+  }
 }
 
 export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  if (req.query.method === 'CREATE') {
+  if (req.method === 'POST') {
     handleCREATE(req, res);
-  } else if (req.query.method === 'DELETE') {
+  } else if (req.method === 'DELETE') {
     handleDELETE(req, res);
-  } else if (req.query.method === 'UPDATE') {
-    handleUPDATE(req, res);
-  } else if (req.query.method === 'READ') {
-    handleREAD(req, res);
+  } else if (req.method === 'PATCH') {
+    handlePATCH(req, res);
+  } else if (req.method === 'GET') {
+    handleGET(req, res);
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`
