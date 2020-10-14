0f71bb71e3585c9843a1f38f4f02e5c40edb7065
Bryanna Gavino <bry_gavino01@berkeley.edu>
2020-10-13 23:58:22 -0700

    style: homepage components situated
    finished placing components where they should be in accordance to figma

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,8 +3,8 @@ import ReactMapGL from 'react-map-gl';
 
 const Map: React.FC = () => {
   const [viewport, setViewport] = useState({
-    width: 100,
-    height: 100,
+    width: 50000000, // large number to fit the box?
+    height: 400,
     latitude: 41.5868,
     longitude: -93.625,
     zoom: 13,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import dynamic from 'next/dynamic';
-import Layout from 'components/Layout';
-import { Button, CircularProgress } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import {
+  Button,
+  CircularProgress,
+  IconButton,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
 // import { signIn, signOut, useSession } from 'next-auth/client';
-// import styles from '../styles/Home.module.css';
+import SearchIcon from '@material-ui/icons/Search';
+import styles from '../styles/Home.module.css';
 
 const MapVisual = dynamic(() => import('../components/Map'), {
   ssr: false,
@@ -10,15 +20,80 @@ const MapVisual = dynamic(() => import('../components/Map'), {
 
 const Home: React.FC = () => {
   return (
-    <Layout title="NBJC Home">
-      <h1>Home</h1>
-      <p>FRED IS THE IMPOSTER!!!!!</p>
-      <Button variant="contained" color="primary">
-        Hello
-      </Button>
-      <CircularProgress />
-      <MapVisual />
-    </Layout>
+    <Box className={styles.pageFlex}>
+      <Box className={styles.pageTitle}>
+        NBJC
+        <Box>
+          <Button
+            className={styles.logButtonSpace}
+            variant="contained"
+            color="primary"
+          >
+            Log In
+          </Button>{' '}
+          <Button
+            className={styles.logButtonSpace}
+            variant="contained"
+            color="primary"
+          >
+            Join Us
+          </Button>
+        </Box>
+      </Box>
+
+      <Box className={styles.pageContent}>
+        <Box className={styles.searchBar}>
+          <Box>
+            <IconButton color="primary">
+              <SearchIcon />
+            </IconButton>
+            <TextField id="standard-basic" label="Explore Organizations" />
+          </Box>
+          <Box className={styles.keywords}>
+            <Box className={styles.keyButtonSpace}>
+              <FormControl className={styles.keyword} variant="outlined">
+                <InputLabel>Keyword</InputLabel>
+                <Select>
+                  <MenuItem value={10}>Test1</MenuItem>
+                  <MenuItem value={20}>Test1</MenuItem>
+                  <MenuItem value={30}>Test1</MenuItem>
+                </Select>
+              </FormControl>{' '}
+            </Box>
+            <Box className={styles.keyButtonSpace}>
+              <FormControl className={styles.keyword} variant="outlined">
+                <InputLabel>Keyword</InputLabel>
+                <Select>
+                  <MenuItem value={10}>Test1</MenuItem>
+                  <MenuItem value={20}>Test1</MenuItem>
+                  <MenuItem value={30}>Test1</MenuItem>
+                </Select>
+              </FormControl>{' '}
+            </Box>
+            <Box className={styles.keyButtonSpace}>
+              <FormControl className={styles.keyword} variant="outlined">
+                <InputLabel>Keyword</InputLabel>
+                <Select>
+                  <MenuItem value={10}>Test1</MenuItem>
+                  <MenuItem value={20}>Test1</MenuItem>
+                  <MenuItem value={30}>Test1</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box className={styles.textBox}>
+          <Box className={styles.text}>SampleText1</Box>
+          <Box className={styles.text}>SampleText2</Box>
+          <Box className={styles.text}>SampleText3</Box>
+        </Box>
+
+        <Box className={styles.component} display="flex">
+          <MapVisual />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
