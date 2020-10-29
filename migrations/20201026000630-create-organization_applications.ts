6bf75de660ccb2d34d5c20cd18ff5acb9890d4d4
Frederick Kim <freddy.kim8@gmail.com>
2020-10-28 19:06:14 -0700

    feat(organization applications): move foreign key to Organizations in relationship with org app
    Created a new migration to add a foreign key column to Organizations table. Changed
    OrganizationApplications table to make most fields nullable.

--- migrations/20201026000630-create-organization_applications.ts
+++ migrations/20201026000630-create-organization_applications.ts
@@ -12,30 +12,27 @@ export function up(db: Base, callback: Base.CallbackFunction): void {
       CREATE TYPE race_demographic AS ENUM ('POC (All)', 'Black', 'Asian', 'Pacific Islander', 'Latinx', 'Native/Indigeneous', 'Other');
       CREATE TYPE age_demographic AS ENUM ('Children', 'Teens', 'Adults', 'Seniors');
       CREATE TABLE organization_applications (
-          id                   SERIAL,
-          application_status   application_status NOT NULL,
-          organization_id      INTEGER NOT NULL,
-          organization_name    VARCHAR(255) NOT NULL,
-          contact_name         VARCHAR(255),
-          contact_email        VARCHAR(255),
-          organization_type    organization_type NOT NULL,
-          work_type            work_type NOT NULL,
-          address              VARCHAR(255) NOT NULL,
-          lat                  DECIMAL NOT NULL,
-          long                 DECIMAL NOT NULL,
-          mission_statement    TEXT,
-          short_history        TEXT,
-          key_values           TEXT,
-          lgbtq_demographic    lgbtq_demographic NOT NULL,
-          race_demographic     race_demographic NOT NULL,
-          age_demographic      age_demographic NOT NULL,
-          capacity             INTEGER,
-          ein                  INTEGER NOT NULL,
-          founding_date        DATE NOT NULL,
-          PRIMARY KEY (id),
-          UNIQUE (organization_id),
-          FOREIGN KEY (organization_id) references organizations(id)
-            );
+        id                   SERIAL,
+        application_status   application_status NOT NULL,
+        organization_name    VARCHAR(255) NOT NULL,
+        contact_name         VARCHAR(255) NOT NULL,
+        contact_email        VARCHAR(255) NOT NULL,
+        organization_type    organization_type,
+        work_type            work_type,
+        address              VARCHAR(255),
+        lat                  DECIMAL,
+        long                 DECIMAL,
+        mission_statement    TEXT,
+        short_history        TEXT,
+        key_values           TEXT,
+        lgbtq_demographic    lgbtq_demographic,
+        race_demographic     race_demographic,
+        age_demographic      age_demographic,
+        capacity             INTEGER,
+        ein                  INTEGER,
+        founding_date        DATE,
+        PRIMARY KEY (id)
+    );
     `,
     callback
   );
