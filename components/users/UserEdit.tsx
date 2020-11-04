2944eff11ff3bbf59e90492e26ae1b81e3977787
Bryanna Gavino <bry_gavino01@berkeley.edu>
2020-11-03 17:12:16 -0800

    feat: staging files

---
+++ components/users/UserEdit.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/users/UserEdit.module.css';
+
+type Props = {
+  title?: string;
+};
+
+const UserEdit: React.FunctionComponent<Props> = () => (
+  <div className={styles.box}>hi</div>
+);
+
+export default UserEdit;
--- pages/users/profile_settings.tsx
+++ pages/users/profile_settings.tsx
@@ -1,11 +1,14 @@
+import { FunctionComponent, useState } from 'react';
 import UsersDrawer from 'components/users/UsersDrawer';
 import Layout from 'components/Layout';
+import UserEdit from 'components/users/UserEdit';
 
-const UserProfSettings: React.FC = () => {
+const UserProfSettings: FunctionComponent<{ initial?: number }> = () => {
+  // const [edit, setEdit] = useState(0);
   return (
     <Layout title="User Profile Settings">
       <UsersDrawer page="User">
-        <p>hi</p>
+        (edit === 0)? <UserEdit /> : undefined;
       </UsersDrawer>
     </Layout>
   );
