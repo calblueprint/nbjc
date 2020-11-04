457df8870d4983c3a06a3d473dd9ce87badbd175
Calvin Chen <chencalvin99@berkeley.edu>
2020-11-03 19:32:38 -0800

    fix(fixing sessions): fixing sessions from fred comment

--- pages/api/sessions.ts
+++ pages/api/sessions.ts
@@ -5,16 +5,17 @@ import CreateError from 'utils/error';
 export default async (
   req: NextApiRequest,
   res: NextApiResponse
-  // eslint-disable-next-line consistent-return
-): Promise<any> => {
+): Promise<void> => {
   const session = await getSession({ req });
   if (session) {
-    res.send({ content: session, message: 'Successfully retrieved session.' });
-  } else {
-    return CreateError(
-      400,
-      'You must be sign in to retrieve sessions on this page.',
-      res
-    );
+    return res.send({
+      content: session,
+      message: 'Successfully retrieved session.',
+    });
   }
+  return CreateError(
+    400,
+    'You must be sign in to retrieve sessions on this page.',
+    res
+  );
 };
