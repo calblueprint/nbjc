55fce7d57fbb62cdfb58f92f7ad959b92f69c631
Frederick Kim <freddy.kim8@gmail.com>
2020-11-08 20:33:06 -0800

    feat(application-qnr-migration): add migrations to create application qnr tables
    Added migrations to create Application Questions and Application Responses tables which will be used
    to create custom questions in the organization applications

---
+++ migrations/20201109040233-create-application_responses.ts
@@ -0,0 +1,50 @@
+import Base from 'db-migrate-base';
+import { promisify } from 'util';
+
+/**
+ * Create the Application Responses table for responses to custom questions
+ */
+export async function up(
+  db: Base,
+  callback: Base.CallbackFunction
+): Promise<void> {
+  db.runSql(
+    `
+    CREATE TABLE application_responses
+      (
+        id                           SERIAL,
+        answer                       TEXT NOT NULL,
+        question_id                  INTEGER NOT NULL,
+        organization_application_id  INTEGER,
+        created_at                   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at                   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        FOREIGN KEY (question_id)
+          REFERENCES application_questions(id),
+        FOREIGN KEY (organization_application_id)
+          REFERENCES organization_applications(id)
+      );
+  `,
+    callback
+  );
+}
+
+/**
+ * Drop the Application Responses table
+ */
+export async function down(
+  db: Base,
+  callback: Base.CallbackFunction
+): Promise<void> {
+  const dropTable = promisify<string>(db.dropTable.bind(db));
+  try {
+    await dropTable('application_responses');
+  } catch (err) {
+    callback(err, null);
+  }
+}
+
+// eslint-disable-next-line no-underscore-dangle
+export const _meta = {
+  version: 1,
+};
