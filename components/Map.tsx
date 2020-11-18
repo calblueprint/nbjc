86477b4d15946063dac8c50cc6e769d7778c9c6a
Frederick Kim <freddy.kim8@gmail.com>
2020-11-17 22:54:10 -0800

    feat(map): add custom styling to map

--- components/Map.tsx
+++ components/Map.tsx
@@ -10,13 +10,13 @@ const Map: React.FunctionComponent<ViewportStateProps> = () => {
   const [viewport, setViewport] = useState<ViewportStateProps | ViewportProps>({
     width: '100%',
     height: '100%',
-    latitude: 41.5868,
-    longitude: -93.625,
-    zoom: 13,
+    latitude: 37.8712,
+    longitude: -122.2601,
+    zoom: 12,
   });
   return (
     <ReactMapGL
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapStyle="mapbox://styles/nbjc-calblueprint/ckhn0b28l04yz1apxewszuevn"
       mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
       onViewportChange={(newViewport) => setViewport(newViewport)}
       {...viewport}
