e2630dfc0c6fe44d3f3990eedc75d22188e7ab05
Frederick Kim <freddy.kim8@gmail.com>
2020-11-21 16:25:03 -0800

    feat(app): update font-family to Montserrat and general typography
    Import Montserrat from Google Fonts and update font-family in Material UI theming and globals.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,11 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <CssBaseline />
       <StylesProvider injectFirst>
--- utils/theme.ts
+++ utils/theme.ts
@@ -16,4 +16,38 @@ export default createMuiTheme({
       main: '#ff0000',
     },
   },
+  typography: {
+    fontFamily: "'Montserrat', 'Helvetica', 'Arial', sans-serif",
+    fontSize: 14,
+    h1: {
+      fontSize: '2.5rem',
+      fontWeight: 800,
+    },
+    h2: {
+      fontSize: '2.25rem',
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: '1.875rem',
+      fontWeight: 600,
+    },
+    h4: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: '0.875rem',
+      fontWeight: 300,
+    },
+    body1: {
+      fontWeight: 400,
+    },
+    body2: {
+      fontWeight: 400,
+    },
+  },
 });
