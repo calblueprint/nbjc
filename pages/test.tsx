2548ffcbace4b6887be57e94509362f92b41711f
Frederick Kim <freddy.kim8@gmail.com>
2021-04-11 18:33:11 -0700

    feat(toast): add toast component

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,8 @@
+// TODO: REMOVE PAGE
 import Link from 'next/link';
 import Layout from 'components/Layout';
-import Toast from 'components/Toast/index';
+import Toast from 'components/Toast';
+import { Button } from '@material-ui/core';
 
 // you don't need to accept incoming changes here
 // 'acceptedAlert','verifyEmailAlert', 'userExistsError'
@@ -12,7 +14,60 @@ const Test: React.FunctionComponent = () => (
         <a>About</a>
       </Link>
     </p>
-    <Toast variant="userExistsError" />
+    {/* <Toast
+      snackbarProps={{
+        anchorOrigin: { vertical: 'top', horizontal: 'center' },
+      }}
+      type="success"
+    >
+      Organization Name has been successfully accepted.
+    </Toast> */}
+    <Toast
+      snackbarProps={{
+        anchorOrigin: { vertical: 'top', horizontal: 'left' },
+        // action: (
+        //   <>
+        //     <Button>hello</Button>
+        //     <Button>testing</Button>
+        //   </>
+        // ),
+        classes: {
+          // root: 'maxWidth: 500',
+        },
+      }}
+      type="success"
+      showDismissButton
+      // dualActionSpaced
+      // disableClickaway
+    >
+      <div>
+        Please check your inbox to verify your email, so you can activate your
+        account and begin the organization application.
+      </div>
+    </Toast>
+    <Toast
+      snackbarProps={{
+        anchorOrigin: { vertical: 'bottom', horizontal: 'left' },
+        // action: (
+        //   <>
+        //     <Button>hello</Button>
+        //     <Button>testing</Button>
+        //   </>
+        // ),
+        classes: {
+          // root: 'maxWidth: 500',
+        },
+      }}
+      type="error"
+      showDismissButton
+      // dualActionSpaced
+      disableClickaway
+    >
+      <div>
+        Please check your inbox to verify your email, so you can activate your
+        account and begin the organization application.
+      </div>
+    </Toast>
   </Layout>
 );
 
