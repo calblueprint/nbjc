5d3c8c9038e4354bbd3583d46e7a0cc862c60963
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-04-12 12:09:43 -0700

    feat: added modal on admin page to invite moderators (#68)
    * feat: added modal on admin page to invite moderators
    
    * Changed onInput to onChange in TextField components
    
    * fix(pages/admin/users.tsx): used onChange in TextField component to prevent deployment issues

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -5,6 +5,15 @@ import Layout from 'components/Layout';
 import { TableUser } from 'interfaces/admin';
 import { GetServerSideProps } from 'next';
 import getSession from 'utils/getSession';
+import {
+  Dialog,
+  DialogActions,
+  DialogContentText,
+  Button,
+  TextField,
+} from '@material-ui/core';
+import { useState } from 'react';
+import styles from '../../styles/admin/Users.module.css';
 
 type AdminUsersIndexProps = {
   users: TableUser[];
@@ -12,19 +21,77 @@ type AdminUsersIndexProps = {
 
 const AdminUsersIndex: React.FunctionComponent<AdminUsersIndexProps> = ({
   users,
-}) => (
-  <Layout title="Admin Users">
-    <AdminIndex
-      page="User"
-      search="Look for a User"
-      // TODO: Add button on click
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      addButtonOnClick={() => {}}
-    >
-      <AdminTable data={users} pageType="users" />
-    </AdminIndex>
-  </Layout>
-);
+}) => {
+  const [openModal, setModal] = useState(false);
+  const [inviteName, setInviteName] = useState('');
+  const [inviteEmail, setInviteEmail] = useState('');
+
+  const handleClickOpen = (): void => {
+    setModal(true);
+  };
+
+  const handleClickClose = (): void => {
+    console.log(inviteEmail);
+    console.log(inviteName);
+    setModal(false);
+  };
+
+  return (
+    <Layout title="Admin Users">
+      <Dialog
+        open={openModal}
+        onClose={handleClickClose}
+        className={styles.newModal}
+        fullWidth
+      >
+        <div className={styles.dialogTitle}> Invite Moderator </div>
+        <div className={styles.modalInput}>
+          <DialogContentText className={styles.modalInputLabel}>
+            Name
+          </DialogContentText>
+          <TextField
+            id="name"
+            variant="outlined"
+            placeholder="FirstName LastName"
+            value={inviteName}
+            onChange={(event) => setInviteName(event.target.value)}
+          />
+        </div>
+        <div className={styles.modalInput}>
+          <DialogContentText className={styles.modalInputLabel}>
+            Email
+          </DialogContentText>
+          <TextField
+            id="email"
+            type="email"
+            variant="outlined"
+            placeholder="email@example.org"
+            value={inviteEmail}
+            onChange={(event) => setInviteEmail(event.target.value)}
+          />
+        </div>
+        <DialogActions>
+          <Button
+            className={styles.inviteButton}
+            onClick={handleClickClose}
+            variant="contained"
+            color="primary"
+          >
+            Invite
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <AdminIndex
+        page="User"
+        search="Look for a User"
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        addButtonOnClick={handleClickOpen}
+      >
+        <AdminTable data={users} pageType="users" />
+      </AdminIndex>
+    </Layout>
+  );
+};
 
 export default AdminUsersIndex;
 
