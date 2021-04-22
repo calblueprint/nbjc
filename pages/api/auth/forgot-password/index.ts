b24dece5cf6d32238392c2908904a21042814771
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-04-21 20:23:45 -0700

    feat(password reset and sendgrid): added sendgrid, password resets

--- pages/api/auth/forgot-password/index.ts
+++ pages/api/auth/forgot-password/index.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient, password_resets } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, PasswordResets } from '@prisma/client';
 import Joi from 'joi';
 import CreateError, { MethodNotAllowed } from 'utils/error';
 import EmailNotifier from 'utils/notify';
@@ -8,77 +8,78 @@ import { NotificationType } from 'utils/notify/types';
 const prisma = new PrismaClient();
 
 export type ForgotPasswordDTO = {
-    email: string,
+  email: string;
 };
 
-export const forgotPassword = async(
-    user: ForgotPasswordDTO
-): Promise<password_resets | null> => {
+export const forgotPassword = async (
+  user: ForgotPasswordDTO
+): Promise<PasswordResets | null> => {
+  const resetUser = await prisma.user.findUnique({
+    where: { email: user.email },
+  });
 
-    const resetUser = await prisma.user.findUnique({
-        where: { email: user.email }
-    });
+  if (!resetUser) {
+    throw new Error(`No account found with email ${user.email}`);
+  }
 
-    if (!resetUser) {
-        throw new Error(`No account found with email ${user.email}`);
+  const resetData = await prisma.passwordResets.create({
+    data: {
+      users: { connect: { email: user.email } },
+    },
+  });
+
+  // Invalidate old reset codes
+  await prisma.passwordResets.updateMany({
+    data: { valid: false },
+    where: {
+      createdAt: { lt: resetData.createdAt },
+      valid: true,
+    },
+  });
+
+  if (!resetData) {
+    return null;
+  }
+
+  // Send email with reset code
+  await EmailNotifier.sendNotification(NotificationType.ForgotPassword, {
+    recipient: user.email,
+    resetCode: resetData.id,
+  });
+
+  return resetData;
+};
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  try {
+    if (req.method !== 'POST') {
+      return MethodNotAllowed(req.method, res);
     }
 
-    const resetData = await prisma.password_resets.create({
-        data: {
-            users: { connect: {email: user.email} },
-        }
+    const expectedBody = Joi.object({
+      email: Joi.string().email().required(),
     });
 
-    // Invalidate old reset codes
-    await prisma.password_resets.updateMany({
-        data: {valid: false},
-        where: {created_at: {lt: resetData.created_at}}
-    });
+    const { value, error } = expectedBody.validate(req.body);
 
-    if (!resetData) {
-        return null;
+    if (error) {
+      return CreateError(400, error.message, res);
     }
-    
-    // Send email with reset code
-    await EmailNotifier.sendNotification(NotificationType.ForgotPassword, {
-        recipient: user.email,
-        resetCode: resetData.id,
-    });
 
-    return resetData;
-}
+    const body = value as ForgotPasswordDTO;
 
-const handler = async(
-    req: NextApiRequest,
-    res: NextApiResponse,
-) : Promise<void> => {
-    try {
-        if (req.method !== 'POST') {
-            return MethodNotAllowed(req.method, res);
-        }
+    const resetData = await forgotPassword(body);
 
-        const expectedBody = Joi.object({
-            email: Joi.string().email().required(),
-        });
-    
-        const { value, error } = expectedBody.validate(req.body);
-    
-        if (error) {
-            return CreateError(400, error.message, res);
-        }
-    
-        const body = value as ForgotPasswordDTO;
-    
-        const resetData = await forgotPassword(body);
-        
-        if (resetData) {
-            return res.status(200).json(resetData);
-        } else {
-            return CreateError(400, 'Could not finish forgot password flow', res);
-        }
-    } catch (err) {
-        return CreateError(500, err.message, res);
+    if (resetData) {
+      return res.status(200).json(resetData);
     }
+    return CreateError(400, 'Could not finish forgot password flow', res);
+  } catch (err) {
+    return CreateError(500, err.message, res);
+  }
 };
 
 export default handler;
--- pages/api/auth/reset-password/index.ts
+++ pages/api/auth/reset-password/index.ts
@@ -1,92 +1,91 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient} from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 import { SanitizedUser } from 'interfaces/user';
 import Joi from 'joi';
 import hashPassword from 'utils/hashPassword';
-import sanitizeUser from "utils/sanitizeUser";
+import sanitizeUser from 'utils/sanitizeUser';
 import CreateError, { MethodNotAllowed } from 'utils/error';
 
 const prisma = new PrismaClient();
 
 export type NewPasswordDTO = {
-    resetCode:   string,
-    newPassword: string,
-}
+  resetCode: string;
+  newPassword: string;
+};
 
-export const resetPassword = async(
-    body: NewPasswordDTO
+export const resetPassword = async (
+  body: NewPasswordDTO
 ): Promise<SanitizedUser> => {
-    if(Joi.string().uuid({ version: "uuidv4" }).validate(body.resetCode).error) {
-        throw new Error("Invalid Reset Code");
+  if (Joi.string().uuid({ version: 'uuidv4' }).validate(body.resetCode).error) {
+    throw new Error('Invalid Reset Code');
+  }
+
+  const resetData = await prisma.passwordResets.findUnique({
+    where: { id: body.resetCode },
+  });
+
+  if (!resetData) {
+    throw new Error('Invalid Reset Code');
+  }
+
+  if (!resetData.valid) {
+    throw new Error('Reset Link/Code has been used or is expired');
+  }
+
+  const updatedPasswordRecord = await prisma.passwordResets.update({
+    data: { valid: false },
+    where: { id: resetData.id },
+  });
+
+  if (!updatedPasswordRecord) {
+    throw new Error('Could not update password');
+  }
+
+  const userRecord = await prisma.user.update({
+    data: { hashedPassword: hashPassword(body.newPassword) },
+    where: { id: updatedPasswordRecord.userId },
+  });
+
+  if (!userRecord) {
+    throw new Error('Could not update password');
+  }
+
+  return sanitizeUser(userRecord);
+};
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  try {
+    if (req.method !== 'POST') {
+      return MethodNotAllowed(req.method, res);
     }
 
-    const resetData = await prisma.password_resets.findUnique({
-        where: {id: body.resetCode},
+    const expectedBody = Joi.object({
+      resetCode: Joi.string().required(),
+      newPassword: Joi.string().min(6).max(50).required().messages({
+        'string.empty': 'Password is required.',
+        'string.min': 'Password must be at least 6 characters.',
+        'string.max': 'Password must be 50 characters or fewer.',
+      }),
     });
 
-    if (!resetData) {
-        throw new Error('Invalid Reset Code');
+    const { value, error } = expectedBody.validate(req.body);
+
+    if (error) {
+      return CreateError(400, error.message, res);
     }
 
-    if (!resetData.valid) {
-        throw new Error('Reset Link/Code has been used or is expired');
+    const body = value as NewPasswordDTO;
+    const updatedUserData = await resetPassword(body);
+    if (updatedUserData) {
+      return res.status(200).json(updatedUserData);
     }
-
-    const updatedPasswordRecord = await prisma.password_resets.update({
-        data:  {valid: false},
-        where: {id: resetData.id},
-    });
-
-    if (!updatedPasswordRecord) {
-        throw new Error('Could not update password');
-    }
-
-    const userRecord = await prisma.user.update({
-        data: { hashedPassword: hashPassword(body.newPassword)},
-        where: {id: updatedPasswordRecord.user_id},
-    });
-
-    if (!userRecord) {
-        throw new Error('Could not update password');
-    }
-    
-    return sanitizeUser(userRecord);
-}
-
-const handler = async(
-    req: NextApiRequest,
-    res: NextApiResponse,
-) : Promise<void> => {
-    try {
-        if (req.method != 'POST') {
-            return MethodNotAllowed(req.method, res);
-        }
-
-        const expectedBody = Joi.object({
-            resetCode:   Joi.string().required(),
-            newPassword: Joi.string().min(6).max(50).required().messages({
-                'string.empty': 'Password is required.',
-                'string.min': 'Password must be at least 6 characters.',
-                'string.max': 'Password must be 50 characters or fewer.',
-              }),
-        });
-    
-        const { value, error } = expectedBody.validate(req.body);
-    
-        if (error) {
-            return CreateError(400, error.message, res);
-        }
-    
-        const body = value as NewPasswordDTO;
-        const updatedUserData = await resetPassword(body);
-        if (updatedUserData) {
-            return res.status(200).json(updatedUserData);
-        } else {
-            return CreateError(404, 'Failed to update password', res);
-        }
-    } catch (err) {
-        return CreateError(500, err.message, res);
-    }
+    return CreateError(404, 'Failed to update password', res);
+  } catch (err) {
+    return CreateError(500, err.message, res);
+  }
 };
 
 export default handler;
