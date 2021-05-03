c1db4b837d56b419bd1900ba23fc204a7e3c0d70
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-05-03 00:29:18 -0700

    Gabe/Moderator Email Invites (#106)
    * Added password reset functionality
    
    * fix: fixed missing files
    
    * feat: password reset/sendgrid
    
    * feat: added sendgrid for automated emailing, started email templates, and added password reset
    
    * feat(password reset and sendgrid): added sendgrid, password resets
    
    * feat(moderator invite): moderator invite api
    
    * feat: implemented moderator invites
    
    * feat: implemented moderator invites and account setup
    
    Co-authored-by: Calvin Chen <chencalvin99@berkeley.edu>

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -14,6 +14,8 @@ import {
 } from '@material-ui/core';
 import { useState } from 'react';
 import styles from '../../styles/admin/Users.module.css';
+import { ModeratorInviteDTO } from 'pages/api/invites/moderator';
+import Toast from 'components/Toast';
 
 type AdminUsersIndexProps = {
   users: TableUser[];
@@ -25,6 +27,36 @@ const AdminUsersIndex: React.FunctionComponent<AdminUsersIndexProps> = ({
   const [openModal, setModal] = useState(false);
   const [inviteName, setInviteName] = useState('');
   const [inviteEmail, setInviteEmail] = useState('');
+  const [submissionError, setSubmissionError] = useState(false);
+  const [errorMessage, setErrorMessage ] = useState('');
+
+  const onInvite = async() : Promise<void> => {
+    setSubmissionError(false);
+    try {
+          if (!inviteEmail) {
+            throw new Error('Please input an email!');
+          }
+          const inviteRes = await fetch('/api/invites/moderator', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json"},
+          credentials: "include",
+          body: JSON.stringify({
+              email: inviteEmail
+          } as ModeratorInviteDTO),
+        });
+
+        if (!inviteRes.ok) {
+          const inviteErr = await inviteRes.json();
+          throw inviteErr.error;
+        }
+    } catch (err) {
+      setErrorMessage(err.message);
+      setSubmissionError(true);
+    }
+    setModal(false);
+    setInviteEmail('');
+    setInviteName('');
+  };
 
   const handleClickOpen = (): void => {
     setModal(true);
@@ -34,51 +66,71 @@ const AdminUsersIndex: React.FunctionComponent<AdminUsersIndexProps> = ({
     setModal(false);
   };
 
+  const renderErrorToast = () : JSX.Element => {
+    return(
+        <Toast
+        snackbarProps={{
+            anchorOrigin: { vertical: 'top', horizontal: 'center' },
+        }}
+        type="error"
+        showDismissButton
+        disableClickaway
+        > 
+        {errorMessage}
+        </Toast>
+    );
+  }
+
+  const inviteModal = () : JSX.Element => (
+    <Dialog
+    open={openModal}
+    onClose={handleClickClose}
+    className={styles.newModal}
+    fullWidth
+    >
+      <div className={styles.dialogTitle}> Invite Moderator </div>
+      <div className={styles.modalInput}>
+        <DialogContentText className={styles.modalInputLabel}>
+          Name
+        </DialogContentText>
+        <TextField
+          id="name"
+          variant="outlined"
+          placeholder="FirstName LastName"
+          value={inviteName}
+          onChange={(event) => setInviteName(event.target.value)}
+        />
+      </div>
+      <div className={styles.modalInput}>
+        <DialogContentText className={styles.modalInputLabel}>
+          Email
+        </DialogContentText>
+        <TextField
+          id="email"
+          type="email"
+          variant="outlined"
+          placeholder="email@example.org"
+          value={inviteEmail}
+          onChange={(event) => setInviteEmail(event.target.value)}
+        />
+      </div>
+      <DialogActions>
+        <Button
+          className={styles.inviteButton}
+          onClick={onInvite}
+          variant="contained"
+          color="primary"
+        >
+          Invite
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+
   return (
     <Layout title="Admin Users">
-      <Dialog
-        open={openModal}
-        onClose={handleClickClose}
-        className={styles.newModal}
-        fullWidth
-      >
-        <div className={styles.dialogTitle}> Invite Moderator </div>
-        <div className={styles.modalInput}>
-          <DialogContentText className={styles.modalInputLabel}>
-            Name
-          </DialogContentText>
-          <TextField
-            id="name"
-            variant="outlined"
-            placeholder="FirstName LastName"
-            value={inviteName}
-            onChange={(event) => setInviteName(event.target.value)}
-          />
-        </div>
-        <div className={styles.modalInput}>
-          <DialogContentText className={styles.modalInputLabel}>
-            Email
-          </DialogContentText>
-          <TextField
-            id="email"
-            type="email"
-            variant="outlined"
-            placeholder="email@example.org"
-            value={inviteEmail}
-            onChange={(event) => setInviteEmail(event.target.value)}
-          />
-        </div>
-        <DialogActions>
-          <Button
-            className={styles.inviteButton}
-            onClick={handleClickClose}
-            variant="contained"
-            color="primary"
-          >
-            Invite
-          </Button>
-        </DialogActions>
-      </Dialog>
+      { submissionError? renderErrorToast() : null}
+      { inviteModal() }
       <AdminIndex
         page="User"
         search="Look for a User"
@@ -112,7 +164,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       },
     };
   } catch (err) {
-    console.log(err);
     return {
       redirect: {
         permanent: false,
